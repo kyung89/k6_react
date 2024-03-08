5ed569d20544163befb126bbc00efa59db08f961
kyung89 <bamtori1029@gmail.com>
2024-03-08 18:02:47 +0900

    2024-03-08 7,8 교시 리액트 시작!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 컴포넌트명은 대문자로 시작! 일반 HTML 태그(소문자로 시작)와의 구분
+// 컴포넌트는 일종의 내가 만드는 태그 
 function App() {
+  // return: 부모 태그 한 개 밖에 안된다! 안의 자식 요소는 몇 개든 상관 없음!
+  // 부모태그 <div></div> 대신 <></> (fragment) 를 사용해도 된다.
+  // class 대신 className 이라고 사용해야 한다. 나중에 html class 로 번역이 된다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            K-digital 6기 옥경림
+          </p>
+        </header>
+      </div>
+      <div>
+        402호
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // App.js 에서 js 생략!
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* App 을 태그처럼 썼음: 사용자 정의 태그 */}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals(console.log);
+
+// 앞으로 컴포넌트를 짤 거임! 컴포넌트는 일종의 함수. 컴포넌트를 사용할 때는 태그 형식으로 사용한다.
